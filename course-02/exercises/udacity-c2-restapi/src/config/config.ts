53f8d26c29faadec1329b33f0810a5d1561db3e6
AbdulRasheed Ilori <54009597+hayyohmyde@users.noreply.github.com>
2022-08-08 13:39:49 +0100

    connecting to database from cli

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,11 +1,23 @@
 export const config = {
+  "postgress": {
+    "username": "devilabay",
+    "password": "devilabay",
+    "database": "ugdb",
+    "host": "ugdb.c1tp2cfq4l0b.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres"
+  },
+  "aws":{
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "devilabay",
+    "password": "devilabay",
+    "database": "ugdb",
+    "host": "ugdb.c1tp2cfq4l0b.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
     "aws_media_bucket": "udagram-ruttner-dev"
   },
